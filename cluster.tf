42aa4a699bdeb3390e3b756fce23f7de3c49e97f
mastanoo7 <cheppalimastan@gmail.com>
2026-08-12 17:55:40 +0530

    Parse EKS VPC delimiter values

--- cluster.tf
+++ cluster.tf
@@ -1,1 +1,64 @@
-#---------------------------------------------------# AWS EKS cluster#---------------------------------------------------resource "aws_eks_cluster" "eks_cluster" {  count = var.cluster_enable ? 1 : 0  name     = var.cluster_name  role_arn = var.cluster_role_arn  dynamic "vpc_config" {    iterator = vpc_config    for_each = jsondecode(var.cluster_vpc_config)    content {      subnet_ids = lookup(vpc_config.value, "subnet_ids", null)      public_access_cidrs     = lookup(vpc_config.value, "public_access_cidrs", null)      endpoint_private_access = lookup(vpc_config.value, "endpoint_private_access", null)      endpoint_public_access  = lookup(vpc_config.value, "endpoint_public_access", null)      security_group_ids      = lookup(vpc_config.value, "security_group_ids", null)    }  }  enabled_cluster_log_types = var.cluster_enabled_cluster_log_types  version                   = var.cluster_version  dynamic "encryption_config" {    iterator = encryption_config    for_each = var.cluster_encryption_config    content {      resources = lookup(encryption_config.value, "resources", null)      dynamic "provider" {        iterator = provider        for_each = length(keys(lookup(encryption_config.value, "provider", {}))) > 0 ? [lookup(encryption_config.value, "provider", {})] : []        content {          key_arn = lookup(provider.value, "key_arn", null)        }      }    }  }  // Blocks of type "kubernetes_network_config" are not expected here.  // dynamic "kubernetes_network_config" {  //   iterator = kubernetes_network_config  //   for_each = var.cluster_kubernetes_network_config  //   content {  //     service_ipv4_cidr = lookup(kubernetes_network_config.value, "service_ipv4_cidr", null)  //   }  // }  dynamic "timeouts" {    iterator = timeouts    for_each = length(keys(var.cluster_timeouts)) > 0 ? [var.cluster_timeouts] : []    content {      create = lookup(timeouts.value, "create", null)      update = lookup(timeouts.value, "update", null)      delete = lookup(timeouts.value, "delete", null)    }  }  tags = merge({ Name = var.cluster_name }, var.tags )  lifecycle {    create_before_destroy = true    ignore_changes        = []  }  depends_on = []}
+#---------------------------------------------------
+# AWS EKS cluster
+#---------------------------------------------------
+resource "aws_eks_cluster" "eks_cluster" {
+  count = var.cluster_enable ? 1 : 0
+
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids         = compact(split(",", split("|", var.cluster_vpc_config)[0]))
+    security_group_ids = compact(split(",", split("|", var.cluster_vpc_config)[1]))
+  }
+
+  enabled_cluster_log_types = var.cluster_enabled_cluster_log_types
+  version                   = var.cluster_version
+
+  dynamic "encryption_config" {
+    iterator = encryption_config
+    for_each = var.cluster_encryption_config
+
+    content {
+      resources = lookup(encryption_config.value, "resources", null)
+
+      dynamic "provider" {
+        iterator = provider
+        for_each = length(keys(lookup(encryption_config.value, "provider", {}))) > 0 ? [lookup(encryption_config.value, "provider", {})] : []
+
+        content {
+          key_arn = lookup(provider.value, "key_arn", null)
+        }
+      }
+    }
+  }
+
+  // Blocks of type "kubernetes_network_config" are not expected here.
+  // dynamic "kubernetes_network_config" {
+  //   iterator = kubernetes_network_config
+  //   for_each = var.cluster_kubernetes_network_config
+  //   content {
+  //     service_ipv4_cidr = lookup(kubernetes_network_config.value, "service_ipv4_cidr", null)
+  //   }
+  // }
+
+  dynamic "timeouts" {
+    iterator = timeouts
+    for_each = length(keys(var.cluster_timeouts)) > 0 ? [var.cluster_timeouts] : []
+
+    content {
+      create = lookup(timeouts.value, "create", null)
+      update = lookup(timeouts.value, "update", null)
+      delete = lookup(timeouts.value, "delete", null)
+    }
+  }
+
+  tags = merge({ Name = var.cluster_name }, var.tags )
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = []
+  }
+
+  depends_on = []
+}
